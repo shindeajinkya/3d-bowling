49091231ddf97f93a2f8f71c41b55502c35f1d2f
shindeajinkya <ajitshinde635@gmail.com>
2023-09-10 10:30:19 +0530

    chore: loader

--- src/Experience/World/index.ts
+++ src/Experience/World/index.ts
@@ -7,6 +7,8 @@ import Walls from "./Walls";
 import Pins from "./Pins";
 import Bowl from "./Bowl";
 import Camera from "../Camera";
+import Time from "../Utils/Time";
+import { loaderTexture } from "../../utils/loader";
 
 export default class World {
   experience: Experience | null = null;
@@ -18,16 +20,24 @@ export default class World {
   pins?: Pins;
   bowl?: Bowl;
   camera?: Camera;
+  time?: Time;
+  loader?: THREE.Mesh;
 
   constructor() {
     this.experience = new Experience(null);
     this.scene = this.experience.scene;
     this.resources = this.experience.resources;
     this.camera = this.experience.camera;
+    this.time = this.experience.time;
+
+    this.setupLoader();
 
     if (!this.resources) return;
     // load meshes after resources are ready
     this.resources.on("ready", () => {
+      if (this.loader) {
+        this.loader.visible = false;
+      }
       this.floor = new Floor();
       this.walls = new Walls();
       this.pins = new Pins();
@@ -36,8 +46,36 @@ export default class World {
     });
   }
 
+  setupLoader() {
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+
+    const image = new Image();
+    image.src = loaderTexture;
+
+    const texture = new THREE.Texture(image);
+    texture.magFilter = THREE.NearestFilter;
+    texture.minFilter = THREE.LinearFilter;
+    texture.needsUpdate = true;
+
+    const material = new THREE.MeshBasicMaterial({
+      map: texture,
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+
+    this.loader = mesh;
+    this.scene?.add(mesh);
+  }
+
+  updateLoader() {
+    if (!this.loader) return;
+    this.loader.rotation.y += Math.cos(Math.PI) * 0.01;
+    this.loader.position.y = Math.sin(this.time?.elapsed ?? 0) * 1.5;
+  }
+
   update() {
     // this.walls?.update();
+    this.updateLoader();
     this.pins?.update();
     this.bowl?.update();
 
